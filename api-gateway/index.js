24203451cdfca0ab16f9969fb17b916048894a29
JA-Zw <180083508+JA-Zw@users.noreply.github.com>
2025-11-19 16:09:52 +0100

    Deel 1: API-Gateway - Gebaseerd op rimmert's code - Docker nog niet getest

---
+++ api-gateway/index.js
@@ -0,0 +1,21 @@
+const Express = require("express");
+const Dotenv = require("dotenv");
+const IndexRouter = require("./routes/index.js")
+
+const app = Express();
+
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+app.use('/', IndexRouter);
+
+//404
+app.use((req, res) => {
+  res.status(404).send("Hé, deze endpoint bestaat niet!");
+});
+
+const port = parseInt(process.env.PORT) || 3011;
+
+app.listen(port, () => {
+  console.log(`🍿 Express running → PORT ${port}`);
+});
