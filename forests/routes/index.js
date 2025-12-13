fbfa0ab944f52df4240acc475162001e49c8ae7c
LVH <herw0011@hz.nl>
2025-12-13 20:37:24 +0100

    Added routing

--- forests/routes/index.js
+++ forests/routes/index.js
@@ -2,6 +2,7 @@ const Express = require("express");
 const { getForests, getForest, createForest, updateForest, deleteForest, getFoodForestSpecies } = require("../controllers/forestController.js");
 const { getSpeciesMany, getSpecies, createSpecies, updateSpecies, deleteSpecies, getSpeciesFoodForests } = require("../controllers/speciesController.js");
 const { getPlant, updatePlant, deletePlant, createPlant } = require("../controllers/plantController.js");
+const { getMessages, createMessage, updateMessage, deleteMessage } = require("../controllers/messageController.js");
 const router = Express.Router();
 
 router.get("/forests", /* als je middleware nodig hebt (bijv. authenticatie), zou je die hier toe kunnen voegen */getForests);
@@ -23,6 +24,11 @@ router.get("/species/:id", getSpecies);
 router.post("/species", createSpecies);
 router.patch("/species/:id", updateSpecies);
 router.delete("/species/:id", deleteSpecies);
+
+router.get("/messages", getMessages);
+router.post("/messages", createMessage);
+router.patch("/messages/:id", updateMessage);
+router.delete("/messages/:userId/:foodForestId/:createdAt", deleteMessage);
 
 router.get('/', (req, res) => {
     res.send('Je hebt de forests api v1 bereikt!');
